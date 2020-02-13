f147adab337de078aaa511e27312c5daae8cd86a
Srujan Mupparapu <srujanmup@gmail.com>
2020-02-13 08:45:21 -0600

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Mupparapu, Srujan on 2/13/20.
+//  Copyright © 2020 Mupparapu, Srujan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
